3d491d47dd78560b7e6ae8152218d4945d5bcfbf
Meni <menil@MENI.com>
2024-10-13 12:42:39 +0300

    creataid a user Schema

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,15 +2,42 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 import validator from "validator";
 
 export interface IUser extends Document {
-  _id: Types.ObjectId;
-  username: string;
+  user_name: string;
   email: string;
   password: string;
-  classId: Types.ObjectId;
-  role: string;
+  classId?: Types.ObjectId;
+  is_Ateacher: boolean;
   grades?: string[];
 }
 
-const UserSchema = new Schema<IUser>({});
+const UserSchema = new Schema<IUser>({
+  user_name:{
+    type:String,
+    required:[true,"user Name is mandatory"]
+  },
+  email:{
+    type:String,
+    required:[true,"email is mandatory"]
+  },
+  password:{
+    type:String,
+    required:[true,"password is mandatory"]
+  },
+  classId:{
+    type:Schema.Types.ObjectId,
+    required:[false,"classId is not mandatory"]
+  },
+  is_Ateacher:{
+    type:Boolean,
+    default:false
+  },
+  grades:{
+    type:String,
+    required:[false,"grades is not mandatory"]
+  }
+});
 
 export default mongoose.model<IUser>("User", UserSchema);
+
+
+
